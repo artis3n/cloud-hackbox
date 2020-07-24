eeb610e1437c9ad51ae9f541ee5595a9af172642
Artis3n <dev@artis3nal.com>
2020-07-23 23:00:00 -0400

    Terraform fmt

--- kali/terraform/main.tf
+++ kali/terraform/main.tf
@@ -1,20 +1,20 @@
 terraform {
-    required_version = "~> 0.12.0"
+  required_version = "~> 0.12.0"
 
-    # Replace this as appropriate for your backend
-    backend "remote" {
-        hostname = "app.terraform.io"
-        organization = "Artis3nal"
+  # Replace this as appropriate for your backend
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "Artis3nal"
 
-        workspaces {
-            name = "cloud-hackbox-kali"
-        }
+    workspaces {
+      name = "cloud-hackbox-kali"
     }
+  }
 }
 
 provider "aws" {
-    version = "~> 2.70"
+  version = "~> 2.70"
 
-    profile = "default"
-    region = "us-east-1"
+  profile = "default"
+  region  = "us-east-1"
 }
--- kali/terraform/server.tf
+++ kali/terraform/server.tf
@@ -1,71 +1,71 @@
 resource "aws_spot_instance_request" "kali" {
-    ami = data.aws_ami.kali.id
-    spot_type = var.kali_spot_type
-    instance_type = var.kali_instance_type
-    key_name = aws_key_pair.kali.id
-    subnet_id = data.aws_subnet.default_vpc_subnet.id
+  ami           = data.aws_ami.kali.id
+  spot_type     = var.kali_spot_type
+  instance_type = var.kali_instance_type
+  key_name      = aws_key_pair.kali.id
+  subnet_id     = data.aws_subnet.default_vpc_subnet.id
 
-    security_groups = [
-        aws_security_group.ssh_from_home.id
-    ]
+  security_groups = [
+    aws_security_group.ssh_from_home.id
+  ]
 
-    root_block_device {
-        volume_size = var.kali_volume_size
-        encrypted = true
-        kms_key_id = data.aws_kms_key.default_ebs.arn
-    }
+  root_block_device {
+    volume_size = var.kali_volume_size
+    encrypted   = true
+    kms_key_id  = data.aws_kms_key.default_ebs.arn
+  }
 
-    metadata_options {
-        http_endpoint = var.metadata_enabled
-        http_tokens = var.metadata_tokens
-        http_put_response_hop_limit = var.metadata_hop_limit
-    }
+  metadata_options {
+    http_endpoint               = var.metadata_enabled
+    http_tokens                 = var.metadata_tokens
+    http_put_response_hop_limit = var.metadata_hop_limit
+  }
 }
 
 resource "aws_security_group" "ssh_from_home" {
-    name = "SSH from CIDR"
-    description = "allow ssh from a cidr range"
-    vpc_id = data.aws_vpc.default_vpc.id
+  name        = "SSH from CIDR"
+  description = "allow ssh from a cidr range"
+  vpc_id      = data.aws_vpc.default_vpc.id
 
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = [var.ssh_cidr_range]
-    }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.ssh_cidr_range]
+  }
 
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_key_pair" "kali" {
-    key_name = "cloud-hackbox-kali"
-    public_key = var.kali_pubkey
+  key_name   = "cloud-hackbox-kali"
+  public_key = var.kali_pubkey
 }
 
 data "aws_ami" "kali" {
-    owners = ["self"]
-    most_recent = true
-    filter {
-        name = "name"
-        values = ["packer-kali-linux-*"]
-    }
+  owners      = ["self"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["packer-kali-linux-*"]
+  }
 }
 
 data "aws_kms_key" "default_ebs" {
-    key_id = "alias/${var.ebs_kms_key}"
+  key_id = "alias/${var.ebs_kms_key}"
 }
 
 data "aws_vpc" "default_vpc" {
-    default = true
+  default = true
 }
 
 data "aws_subnet" "default_vpc_subnet" {
-    vpc_id = data.aws_vpc.default_vpc.id
-    default_for_az = true
-    availability_zone = var.vpc_az
+  vpc_id            = data.aws_vpc.default_vpc.id
+  default_for_az    = true
+  availability_zone = var.vpc_az
 }
