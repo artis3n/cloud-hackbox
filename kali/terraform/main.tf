f289ded8bfa517e08557179208c3b4c518b5bfd8
Artis3n <dev@artis3nal.com>
2021-04-18 18:45:33 -0400

    Fix broken CI

--- kali/terraform/main.tf
+++ kali/terraform/main.tf
@@ -1,10 +1,9 @@
 terraform {
-  required_version = "~> 0.14.0"
+  required_version = "~> 0.15.0"
 
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "3.22.0"
+      source = "hashicorp/aws"
     }
   }
 
